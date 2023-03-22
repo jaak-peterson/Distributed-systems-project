15442e0d0ddd38708a5532529e77fe592bf2a55b
Herman Rull <rullherman@gmail.com>
2023-03-22 12:49:51 +0200

    everything up to game start works.

--- node.py
+++ node.py
@@ -7,8 +7,6 @@ import os
 import tictactoe_pb2
 import tictactoe_pb2_grpc
 
-LEADER_ID = 3
-
 network="localhost"
 
 port_map = {
@@ -42,26 +40,38 @@ class Node(tictactoe_pb2_grpc.TicTacToeServicer):
         return tictactoe_pb2.PingResult(success=True)
 
     def handle_election(self, request, context):
-        print(f"Received election message from process {request.sender_id}")
-        if self.node_id == LEADER_ID:
-            result = tictactoe_pb2.ElectionResult()
-            result.leader_id = LEADER_ID
+
+        print(f"Received election message from process {request.candidate_ids[-1]}")
+        if self.node_id == request.candidate_ids[0]:
+            result = tictactoe_pb2.ElectionResult(leader_id = max(request.candidate_ids))
             result.success = True
             return result
         else:
-            print(f"Forwarding election message from process {request.sender_id} to process {request.sender_id + 1}")
-            with grpc.insecure_channel(f'localhost:50053') as channel:
+            target_id = self.node_id + 1
+            if target_id > 3:
+                target_id -= 3
+            print(f"Forwarding election message from process {request.candidate_ids[-1]} to process {target_id}")
+            with grpc.insecure_channel(f'{network}:{port_map[target_id]}') as channel:
                 stub = tictactoe_pb2_grpc.TicTacToeStub(channel)
-                response = stub.handle_election(tictactoe_pb2.ElectionMessage(sender_id=request.sender_id + 1))
+                new_c_ids = list(request.candidate_ids) + [self.node_id]
+                response = stub.handle_election(tictactoe_pb2.ElectionMessage(candidate_ids=new_c_ids))
+                if response.success:
+                    print(f'Leader selected: {response.leader_id}')
+                    self.leader_id = response.leader_id
                 return response
 
     def start_election(self):
         print(f"I am process {self.node_id} and I am initiating the election")
-        with grpc.insecure_channel(f'localhost:50052') as channel:
+
+        target_id = self.node_id + 1
+        if target_id>3:
+            target_id -= 3
+        with grpc.insecure_channel(f'{network}:{port_map[target_id]}') as channel:
             stub = tictactoe_pb2_grpc.TicTacToeStub(channel)
-            response = stub.handle_election(tictactoe_pb2.ElectionMessage(sender_id=1))
+            response = stub.handle_election(tictactoe_pb2.ElectionMessage(candidate_ids=[self.node_id]))
             if response.success:
                 print(f"Node {self.node_id}: Election completed successfully. Coordinator ID is {response.leader_id}")
+                self.leader_id=response.leader_id
             else:
                 print("Election failed")
             return response
@@ -86,6 +96,21 @@ class Node(tictactoe_pb2_grpc.TicTacToeServicer):
                 response = stub.handle_opponent_move(tictactoe_pb2.MoveMessage(node_id=1, move=move_str, state=state))
 
         return tictactoe_pb2.MoveResponse(state=state)
+
+    def is_connected_with_other_nodes(self):
+        connected_to_all_nodes = True
+        print("Checking connection with other nodes.")
+        for target in port_map.keys():
+            if id != target:
+                if not check_ping(target):
+                    connected_to_all_nodes = False
+                    print(f'Connection to node{target} failed trying again in 10 seconds')
+                    # Necessary for case when elected leader disconnects.
+                    if (target == self.leader_id):
+                        self.leader_id = None
+
+        return connected_to_all_nodes
+
 
     def handle_opponent_move(self, request, context):
         state = request.state
@@ -126,20 +151,28 @@ def check_ping(target_id):
 def serve(id):
     node = Node(id, port_map[id])
     node.run_server()
-    while True:
-        connected_to_all_nodes = True
-        print("Checking connection with other nodes.")
-        for target in port_map.keys():
-            if id != target:
-                if not check_ping(target):
-                    connected_to_all_nodes = False
-                    print(f'Connection to node{target} failed trying again in 10 seconds')
-        if connected_to_all_nodes:
-            break
-        time.sleep(10)
 
-    node.start_election()
-    node.init_game()
+    while True:
+        if not node.is_connected_with_other_nodes():
+            time.sleep(10)
+            continue
+
+        if not node.leader_id:
+            success = node.start_election()
+            if not success:
+                time.sleep(10)
+                continue
+
+        if node.leader_id == id:
+            if not node.game:
+                node.init_game()
+            time.sleep(5)
+            continue
+        else:
+
+
+
+
     """
     time.sleep(3)
     # Play game
--- tictactoe_pb2.py
+++ tictactoe_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0ftictactoe.proto\x12\x0btic_tac_toe\"\x07\n\x05\x45mpty\"$\n\x0f\x45lectionMessage\x12\x11\n\tsender_id\x18\x01 \x01(\x05\"4\n\x0e\x45lectionResult\x12\x11\n\tleader_id\x18\x01 \x01(\x05\x12\x0f\n\x07success\x18\x02 \x01(\x08\"\x1d\n\nPingResult\x12\x0f\n\x07success\x18\x01 \x01(\x08\";\n\x0bMoveMessage\x12\x0f\n\x07node_id\x18\x01 \x01(\x05\x12\x0c\n\x04move\x18\x02 \x01(\t\x12\r\n\x05state\x18\x03 \x01(\x08\"\x1d\n\x0cMoveResponse\x12\r\n\x05state\x18\x01 \x01(\x08\x32\xa5\x02\n\tTicTacToe\x12:\n\tping_node\x12\x12.tic_tac_toe.Empty\x1a\x17.tic_tac_toe.PingResult\"\x00\x12N\n\x0fhandle_election\x12\x1c.tic_tac_toe.ElectionMessage\x1a\x1b.tic_tac_toe.ElectionResult\"\x00\x12\x44\n\x0bhandle_move\x12\x18.tic_tac_toe.MoveMessage\x1a\x19.tic_tac_toe.MoveResponse\"\x00\x12\x46\n\x14handle_opponent_move\x12\x18.tic_tac_toe.MoveMessage\x1a\x12.tic_tac_toe.Empty\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0ftictactoe.proto\x12\x0btic_tac_toe\"\x07\n\x05\x45mpty\"(\n\x0f\x45lectionMessage\x12\x15\n\rcandidate_ids\x18\x01 \x03(\x05\"4\n\x0e\x45lectionResult\x12\x11\n\tleader_id\x18\x01 \x01(\x05\x12\x0f\n\x07success\x18\x02 \x01(\x08\"\x1d\n\nPingResult\x12\x0f\n\x07success\x18\x01 \x01(\x08\";\n\x0bMoveMessage\x12\x0f\n\x07node_id\x18\x01 \x01(\x05\x12\x0c\n\x04move\x18\x02 \x01(\t\x12\r\n\x05state\x18\x03 \x01(\x08\"\x1d\n\x0cMoveResponse\x12\r\n\x05state\x18\x01 \x01(\x08\x32\xa5\x02\n\tTicTacToe\x12:\n\tping_node\x12\x12.tic_tac_toe.Empty\x1a\x17.tic_tac_toe.PingResult\"\x00\x12N\n\x0fhandle_election\x12\x1c.tic_tac_toe.ElectionMessage\x1a\x1b.tic_tac_toe.ElectionResult\"\x00\x12\x44\n\x0bhandle_move\x12\x18.tic_tac_toe.MoveMessage\x1a\x19.tic_tac_toe.MoveResponse\"\x00\x12\x46\n\x14handle_opponent_move\x12\x18.tic_tac_toe.MoveMessage\x1a\x12.tic_tac_toe.Empty\"\x00\x62\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'tictactoe_pb2', globals())
@@ -23,15 +23,15 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _EMPTY._serialized_start=32
   _EMPTY._serialized_end=39
   _ELECTIONMESSAGE._serialized_start=41
-  _ELECTIONMESSAGE._serialized_end=77
-  _ELECTIONRESULT._serialized_start=79
-  _ELECTIONRESULT._serialized_end=131
-  _PINGRESULT._serialized_start=133
-  _PINGRESULT._serialized_end=162
-  _MOVEMESSAGE._serialized_start=164
-  _MOVEMESSAGE._serialized_end=223
-  _MOVERESPONSE._serialized_start=225
-  _MOVERESPONSE._serialized_end=254
-  _TICTACTOE._serialized_start=257
-  _TICTACTOE._serialized_end=550
+  _ELECTIONMESSAGE._serialized_end=81
+  _ELECTIONRESULT._serialized_start=83
+  _ELECTIONRESULT._serialized_end=135
+  _PINGRESULT._serialized_start=137
+  _PINGRESULT._serialized_end=166
+  _MOVEMESSAGE._serialized_start=168
+  _MOVEMESSAGE._serialized_end=227
+  _MOVERESPONSE._serialized_start=229
+  _MOVERESPONSE._serialized_end=258
+  _TICTACTOE._serialized_start=261
+  _TICTACTOE._serialized_end=554
 # @@protoc_insertion_point(module_scope)
